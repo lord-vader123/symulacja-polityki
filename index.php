46c72d99f3c2af4d4dcca184a32c4b600cef3127
Szymon P <szymon_jozef@proton.me>
2024-10-14 14:35:24 +0200

    Dodano szablon pliku

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Symulacja polityki</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+
+<?php include_once __DIR__ . './snippets/header-logged.html'; ?>
+    
+<div class="content">
+
+</div>
+
+<?php include_once __DIR__ . './snippets/footer.html' ?>
+</body>
+</html>
