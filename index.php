4d321b0fa33def6f038822dd7c920c989ced1721
Szymon P <szymon_jozef@proton.me>
2024-10-14 14:38:54 +0200

    Dodano tralalala

--- index.php
+++ index.php
@@ -12,6 +12,26 @@
     
 <div class="content">
 
+<p>
+    <h1>
+        Witaj na stronie: "Symulacja polityki"
+    </h1>
+</p>
+
+<p>
+    <h2>
+        Zapytasz pewnie: a co tu się robi? Już śpieszę z wyjaśnieniem!
+    </h2>
+    <ul>
+        <li>Tworzysz się konto</li>
+        <li>Loguje się na nie</li>
+        <li>Tworzy się swoją partie, swoich polityków i komitet wyborczy</li>
+        <li>Przeprowadza się symulację wyborów</li>
+    </ul>
+</p>
+<p>
+    Liczymy na to, że będziesz się dobrze bawił!
+</p>
 </div>
 
 <?php include_once __DIR__ . './snippets/footer.html' ?>
