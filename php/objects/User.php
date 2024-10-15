84460c79956261b7a68569f33c2368dabe461dcb
Szymon <98268045+lord-vader123@users.noreply.github.com>
2024-10-15 10:27:06 +0200

    Dodano gettery i settery

--- php/objects/User.php
+++ php/objects/User.php
@@ -79,4 +79,12 @@ class User {
 
         return $stmt->execute();
     }
+
+    public function setData(array $data) : void {
+        $this->data = $data;
+    }
+
+    public function getData() : array {
+        return $this->data;
+    }
 }
