ac9d1a480bf0673802b5389a933d754419ef5f57
Szymon <98268045+lord-vader123@users.noreply.github.com>
2024-10-15 11:11:20 +0200

    Zaimplementowano getDataFromDb

--- php/objects/Polityk.php
+++ php/objects/Polityk.php
@@ -6,7 +6,19 @@ class Polityk extends Table {
     }
 
     public function getDataFromDb(int $id): bool {
+        if (!$id && !isset($id)) {
+            return false;
+        } 
 
+        $stmt = $this->conn->prepare("SELECT * FROM polityk WHERE id = ?");
+        $stmt->bind_param("i", $id);
+        if (!$stmt->execute()) {
+            throw new Exception("Błąd podczas pobierania danych z bazy danych");
+        }
+        
+        $this->data = $stmt->get_result()->fetch_assoc();
+
+        return true;
     }
 
     public function insertDataToDb(): bool {
