801d1f3b76a8dd2c956cd37f9ab9b42af958c797
Szymon P <szymon_jozef@proton.me>
2024-11-07 21:07:14 +0100

    Bakend

--- forms/partia.php
+++ forms/partia.php
@@ -1,13 +1,20 @@
+<?php
+include_once __DIR__ . '/../php/login-database.php';
+include_once __DIR__ . '/../php/objects/Partia.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Partia</title>
     <link rel="stylesheet" href="../css/style.css">
 </head>
+
 <body>
-<div class="content">
+    <div class="content">
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
             <label for="nazwa">nazwa partii</label>
             <input type="text" name="nazwa" id="nazwa">
             <label for="skrot">podaj skróconą nazwę partii</label>
@@ -15,6 +22,24 @@
             <label for="logo_src">tu logo partii :) </label>
             <input type="file" name="logo_src" id="logo_src">
             <button type="submit">tu zatwierdź</button>
-</div>
+            <?php
+            if ($_SERVER['REQUEST_METHOD'] == "POST") {
+                $partia = new Partia($conn, null);
+                $data = [
+                    "nazwa" => $conn->real_escape_string($_POST["nazwa"]),
+                    "skrot" => $conn->real_escape_string($_POST["skrot"]),
+                    "logo_src" => $conn->real_escape_string($_POST["logo_src"]),
+                ];
+                $partia->setData($data);
+                if ($partia->insertDataToDb()) {
+                    echo "Poprawnie :D";
+                } else {
+                    echo "no nie :(";
+                }
+            }
+            ?>
+        </form>
+    </div>
 </body>
+
 </html>
--- php/objects/Partia.php
+++ php/objects/Partia.php
@@ -3,31 +3,35 @@
 /**
  * Klasa do obsługi tabeli "Partia" w bazie danych.
  */
-class Partia extends Table {
-    public function __construct(mysqli $conn, ?int $id = null) {
+class Partia extends Table
+{
+    public function __construct(mysqli $conn, ?int $id = null)
+    {
         parent::__construct($conn, $id);
     }
 
-    public function getDataFromDb(int $id): bool {
+    public function getDataFromDb(int $id): bool
+    {
         if (!$id && !isset($id)) {
             return false;
-        } 
+        }
 
         $stmt = $this->conn->prepare("SELECT * FROM partia WHERE id = ?");
         $stmt->bind_param("i", $id);
         if (!$stmt->execute()) {
             throw new Exception("Błąd podczas pobierania danych z bazy danych");
         }
-        
+
         $this->data = $stmt->get_result()->fetch_assoc();
         return true;
     }
 
-    public function insertDataToDb(): bool {
-        if (count($this->data) !== 3 ) {
+    public function insertDataToDb(): bool
+    {
+        if (count($this->data) !== 3) {
             throw new Exception("Błędna ilość argumentów");
         }
-        
+
         $stmt = $this->conn->prepare("INSERT INTO partia(nazwa, skrot, logo_src) VALUES(?, ?, ?)");
         if (!$stmt) {
             throw new Exception("Błąd podczas przygotowywania kwerendy");
