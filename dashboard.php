27ecfe33ddad23e5ffb72ce1b9f379fb650e3f00
Szymon P <szymon_jozef@proton.me>
2024-11-07 18:04:20 +0100

    XDDDDDDDDDD

--- dashboard.php
+++ dashboard.php
@@ -1,20 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
     <title>Zalogowany jako: </title>
 </head>
+
 <body>
 
-<?php include_once __DIR__ . './snippets/header-logged.html'; ?>
-    
-<div class="content">
+    <?php include_once __DIR__ . '/snippets/header-logged.html'; ?>
 
-</div>
+    <div class="content">
 
-<?php include_once __DIR__ . './snippets/footer.html'; ?>
+    </div>
+
+    <?php include_once __DIR__ . '/snippets/footer.html'; ?>
 
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -1,41 +1,44 @@
 <!DOCTYPE html>
 <html lang="pl">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Symulacja polityki</title>
     <link rel="stylesheet" href="./css/style.css">
 </head>
+
 <body>
 
-<?php include_once __DIR__ . './snippets/header.html'; ?>
+    <?php include_once __DIR__ . '/snippets/header.html'; ?>
 
-<div class="main">
-    <div class="content">
+    <div class="main">
+        <div class="content">
 
-    <p>
-        <h1>
-            Witaj na stronie: "Symulacja polityki"
-        </h1>
-    </p>
+            <p>
+            <h1>
+                Witaj na stronie: "Symulacja polityki"
+            </h1>
+            </p>
 
-    <p>
-        <h2>
-            Zapytasz pewnie: a co tu się robi? Już śpieszę z wyjaśnieniem!
-        </h2>
-        <ul>
-            <li>Tworzysz się konto</li>
-            <li>Loguje się na nie</li>
-            <li>Tworzy się swoją partie, swoich polityków i komitet wyborczy</li>
-            <li>Przeprowadza się symulację wyborów</li>
-        </ul>
-    </p>
-    <p>
-        Liczymy na to, że będziesz się dobrze bawił!
-    </p>
+            <p>
+            <h2>
+                Zapytasz pewnie: a co tu się robi? Już śpieszę z wyjaśnieniem!
+            </h2>
+            <ul>
+                <li>Tworzysz się konto</li>
+                <li>Loguje się na nie</li>
+                <li>Tworzy się swoją partie, swoich polityków i komitet wyborczy</li>
+                <li>Przeprowadza się symulację wyborów</li>
+            </ul>
+            </p>
+            <p>
+                Liczymy na to, że będziesz się dobrze bawił!
+            </p>
+        </div>
+
+        <?php include_once __DIR__ . '/snippets/footer.html' ?>
     </div>
-
-    <?php include_once __DIR__ . './snippets/footer.html' ?>
-</div>
 </body>
+
 </html>
