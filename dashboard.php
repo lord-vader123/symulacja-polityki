fba6e3098c1047cc0da3759f88e567b90af8cddb
Pandorciaa <matuszak.martyna@o2.pl>
2024-11-08 23:39:16 +0100

    jesjes

--- dashboard.php
+++ dashboard.php
@@ -22,7 +22,13 @@ if (!isset($_COOKIE['login']) && !isset($_COOKIE['password'])) {
     <?php include_once __DIR__ . '/snippets/header-logged.html'; ?>
 
     <div class="content">
-        <p>Cześć <?php echo $_COOKIE['login']; ?>!</p>
+        <h1>Witaj <?php echo $_COOKIE['login']; ?>!</h1>
+        <p>Teraz możesz zacząć zabawę w naszej symulacji!</p>
+        <p>W narzędziach znajdziesz dwie zakładki:</p>
+        <p>1. Dodaj polityka - podajesz imie, nazwisko, nazwe partii (po dodaniu partii za pomocą zakładki numer 2) oraz dodajesz zdjęcie polityka.</p>
+        <p>2. Dodaj partię - podajesz nazwę partii, skrót oraz dodajesz zdjęcie loga twojej partii.</p>
+        <p>Po wykonaniu tych czynności możesz śmiało wejść w "Symuluj!"</p>
+        <p>Dobrej zabawy! ♥</p>
     </div>
 
     <?php include_once __DIR__ . '/snippets/footer.html'; ?>
