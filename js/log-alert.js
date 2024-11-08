f7e85eb3efb28a798e1b514e77c5fba37e5f0ac8
Szymon P <szymon_jozef@proton.me>
2024-11-08 09:37:22 +0100

    obsłużono więcej opcji

--- js/log-alert.js
+++ js/log-alert.js
@@ -1,5 +1,10 @@
 function notLogged(event) {
     event.preventDefault();
+    if (window.location.href = "/symulacja-polityki/forms/user.php") {
+        alert("Musisz się zalogować, by uzyskać dostęp do tej funkcji");
+        console.log("Już tu jestem hehehhe");
+        return;
+    }
     if (confirm("Aby uzyskać dostęp do tej funkcji musisz być zalogowany. Przenieść do strony logowania?")) {
         window.location.href = "/symulacja-polityki/forms/user.php";
         console.log("PRZENIEŚ MNIE SŁODZIAKU");
