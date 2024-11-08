65ea91587f039db064504eaedae09bf7d5e8d33d
Szymon P <szymon_jozef@proton.me>
2024-11-08 09:18:42 +0100

    naprawiono skrypt

--- js/log-alert.js
+++ js/log-alert.js
@@ -1,4 +1,9 @@
-function notLogged(){
-    alert("Aby uzyskać dostęp do tej funkcji musisz być zalogowany")
-    window.location.href = "php/login.php";
+function notLogged(event) {
+    event.preventDefault();
+    if (confirm("Aby uzyskać dostęp do tej funkcji musisz być zalogowany. Przenieść do strony logowania?")) {
+        window.location.href = "/symulacja-polityki/forms/user.php";
+        console.log("PRZENIEŚ MNIE SŁODZIAKU");
+        return;
+    }
+    console.log("Nie zapragnąłem zostać przeniesionym");
 }
